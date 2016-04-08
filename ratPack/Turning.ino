80e78ff57365cc26edbf9b8be04ae7e3a773dc79
Dan Clark <dpclark.ub@gmail.com>
2016-04-08 15:43:51 -0400

    removed compilation error

--- ratPack/Turning.ino
+++ ratPack/Turning.ino
@@ -1,17 +1,3 @@
-#include "Motors.h"
-#include "Sensors.h"
-#include "Gyro.h"
-
-void loop() {
-  //read sensor values until there are no walls on one or more sides
-  readSensors();
-  if(leftSensor < 300){
-    turnLeft();
-  } else if (rightSensor < 300){
-    turnRight();
-  }
-}
-
 void turnRight(){
   int startAngle = angle;
   //do until gyroscope reads 90
@@ -19,7 +5,7 @@ void turnRight(){
   {  //make left wheels turn
       setLeftPWM(150);
       //right wheels backward
-      setRightPWM(-150)
+      setRightPWM(-150);
   }while (angle < (90+startAngle));
   setLeftPWM(0);
   setRightPWM(0);
@@ -30,9 +16,9 @@ void turnLeft(){
   //do until gyroscope reads 90
   do
   {   //make right wheels turn
-      setRightPWM(150) 
+      setRightPWM(150) ;
       //left wheels backward
-      setLeftPWM(-150)
+      setLeftPWM(-150);
   }while (angle > (startAngle-90));
   setLeftPWM(0);
   setRightPWM(0);
